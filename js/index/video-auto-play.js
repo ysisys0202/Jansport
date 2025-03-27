29f13a33ad37ce60bd466f7a8f10f32f8bf1e3e9
이윤슬 <ysisys0202@gmail.com>
2025-03-28 05:26:32 +0900

    🐛 유튜브 영상 교체

--- js/index/video-auto-play.js
+++ js/index/video-auto-play.js
@@ -9,10 +9,11 @@
     if (flag && window.pageYOffset > startPosition) {
       jansportVideo.setAttribute(
         "src",
-        "https://www.youtube.com/embed/QrWG7q4Epi8?autoplay=1&mute=1;",
+        "https://www.youtube.com/embed/2y24JbAnirc?si=zb-GQV1wqFPJ6Q2l&autoplay=1&mute=1"
       );
       flag = false;
     }
+    console.log(flag);
   };
   window.addEventListener("scroll", scrollHandler);
 }
